219be87499b3cb8d32df224a4ad32a35a55211e0
HlebushekBrave <73088409+HlebushekBrave@users.noreply.github.com>
2020-10-24 20:23:59 +0700

    Update turtle.py

--- venv/turtle.py
+++ venv/turtle.py
@@ -3,23 +3,54 @@
 #               Raspopova S. (%),
 #               Fauzi A. (%)
 
-
 import turtle
 
 
-def square(x, y, side, color):
-    '''
+def parallelogram(x, y, a, b, color1, color2):
+
+    """
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: first side length of a parallelogram
+    :param b: second side length of a parallelogram
+    :param color1: parallelogram border color
+    :param color2: parallelogram inner color
+    :return: None
+    """
+
+    turtle.color(color1, color2)
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.left(45)
+    turtle.forward(b)
+    turtle.left(135)
+    turtle.forward(a)
+    turtle.left(45)
+    turtle.forward(b)
+    turtle.left(135)
+    turtle.end_fill()
+    turtle.penup()
+
+
+def square(x, y, side, color1, color2):
+
+    """
     Function, drawing square.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
     :param side: side length of a square
-    :param color: square's color
+    :param color1: square's border color
+    :param color2: square's inner color
     :return: None
-    '''
+    """
+
     turtle.up()
     turtle.goto(x, y)
     turtle.begin_fill()
-    turtle.color(color, color)
+    turtle.color(color1, color2)
     turtle.down()
     turtle.forward(side)
     turtle.left(90)
@@ -28,15 +59,22 @@ def square(x, y, side, color):
     turtle.forward(side)
     turtle.left(90)
     turtle.forward(side)
+    turtle.left(90)
     turtle.end_fill()
 
+
 def main():
-    '''
-    Main function.
+
+    """
+    Main function
     :return: None
-    '''
-    square(0, 0, 180, "blue")
- 
+    """
+
+    parallelogram(0, 0, 100, 60, "black", "orange")
+    square(200, 200, 100, "black", "blue")
     turtle.done()
+    turtle.mainloop()
+
+
 if __name__ == '__main__':
     main()
